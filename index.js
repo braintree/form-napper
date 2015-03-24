d435571aee89cd8830f94248eb4568819d3ab5b7
mrak and jeff <code@getbraintree.com>
2015-03-24 00:08:01 +0000

    Adding form-napper and tests

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 'use strict';
 
-var LibraryToExport = require('./src/sample-lib');
-
-function create() {
-  return new LibraryToExport();
+function FormNapper(formId) {
+  this.htmlForm = document.getElementById(formId);
 }
 
-module.exports = create;
+FormNapper.prototype.hijack = function (onsubmit) {
+  function handler(e) {
+    e.preventDefault();
+    onsubmit(e);
+  }
+
+  if (global.addEventListener != null) {
+    this.htmlForm.addEventListener('submit', handler);
+  } else if (global.attachEvent != null) {
+    this.htmlForm.attachEvent('onsubmit', handler);
+  } else {
+    this.htmlForm.onsubmit = handler;
+  }
+};
+
+FormNapper.prototype.inject = function (name, value) {
+  var input;
+
+  if (this.htmlForm[name] != null && 'value' in this.htmlForm[name]) {
+    input = this.htmlForm[name];
+  } else {
+    input = document.createElement('input');
+    this.htmlForm.appendChild(input);
+  }
+
+  input.name = name;
+  input.value = value;
+};
+
+FormNapper.prototype.submit = function () {
+  var form = this.htmlForm;
+
+  if (form.submit && (typeof form.submit === 'function' || form.submit.call)) {
+    form.submit();
+  } else {
+    setTimeout(function () {
+      form.querySelector('[type="submit"]').click();
+    }, 1);
+  }
+};
+
+module.exports = FormNapper;
